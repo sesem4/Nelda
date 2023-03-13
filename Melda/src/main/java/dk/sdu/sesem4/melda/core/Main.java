7e03e71294fa691aceb0ca56c812f7c8a08f9504
JfriisJ <40535086+JfriisJ@users.noreply.github.com>
2023-03-13 12:58:57 +0100

    Changes by muham

--- Melda/src/main/java/dk/sdu/sesem4/melda/core/Main.java
+++ Melda/src/main/java/dk/sdu/sesem4/melda/core/Main.java
@@ -1,5 +1,8 @@
 package dk.sdu.sesem4.melda.core;
 
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
 public class Main {
     public static void main(String[] args) {
         LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
