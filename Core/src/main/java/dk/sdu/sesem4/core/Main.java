bd97d250ec1dfb0c34b9ed3367c6c121f3d66663
JfriisJ <40535086+JfriisJ@users.noreply.github.com>
2023-03-28 12:19:59 +0200

    Created a test for Rendering map and player

--- Core/src/main/java/dk/sdu/sesem4/core/Main.java
+++ Core/src/main/java/dk/sdu/sesem4/core/Main.java
@@ -1,8 +1,17 @@
 package dk.sdu.sesem4.core;
 
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setForegroundFPS(60);
+        config.setResizable(true);
+        config.setTitle("Nelda");
+        new Lwjgl3Application(new Game(), config);
         
     }
 }
