7d2c8fde1302b1aa537591a0df25a99248376ac3
JfriisJ <40535086+JfriisJ@users.noreply.github.com>
2023-03-28 11:11:27 +0200

    All the map tiles and the need files has been put into the resource folder.

---
+++ Map/src/main/resources/SpriteSheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="SpriteSheet" tilewidth="16" tileheight="16" tilecount="126" columns="7">
+    <image source="SpriteSheet.png" trans="ff00ff" width="112" height="288"/>
+</tileset>
