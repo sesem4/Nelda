c97a39a9b297af0405ecc74d8f71dfb60276699e
jakob753951 <jlm1999@live.dk>
2023-03-29 10:32:39 +0200

    Added whole map, and moved Zelda.png

--- Map/src/main/resources/Nelda.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.0" name="Nelda" tilewidth="16" tileheight="16" tilecount="22528" columns="256">
- <image source="Nelda.png" width="4096" height="1408"/>
-</tileset>
