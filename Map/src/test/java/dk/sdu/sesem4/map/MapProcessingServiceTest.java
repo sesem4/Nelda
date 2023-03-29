dcca9d0ded2b3fc34ec67b3f88a0790b7e19da10
jakob753951 <jlm1999@live.dk>
2023-03-29 13:30:15 +0200

    added MapProcessingServiceTest and GdxTestRunner

--- Map/src/test/java/dk/sdu/sesem4/map/MapProcessingServiceTest.java
+++ Map/src/test/java/dk/sdu/sesem4/map/MapProcessingServiceTest.java
@@ -4,16 +4,19 @@ import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTile;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * This test makes sure that a map is loaded into the array and returns the array of maps.
  */
-class MapProcessingServiceTest {
+@RunWith(GdxTestRunner.class)
+public class MapProcessingServiceTest {
     @Test
-    void loadWorld() {
+    public void loadWorld() {
         // load map
-        String fileName = "A1.tmx";
+        String fileName = "overworld/A1.tmx";
         TmxMapLoader tmxMapLoader = new TmxMapLoader();
         TiledMap map = tmxMapLoader.load(fileName);
 
@@ -22,8 +25,12 @@ class MapProcessingServiceTest {
         TiledMapTileLayer.Cell cell = layer.getCell(0, 0);
         TiledMapTile tile = cell.getTile();
         int id = tile.getId();
+        int id2 = layer.getCell(7, 4).getTile().getId();
+        int id3 = layer.getCell(15, 4).getTile().getId();
 
-        // make sure the id is correct
-        assert id == 12;
+        // make sure the ids are correct
+        assertEquals(id, 12);
+        assertEquals(id2, 34);
+        assertEquals(id3, 3);
     }
 }
