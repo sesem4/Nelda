811cec98789e2d65b52162220774a481247636c4
AnneLaerke <91538760+AnneLaerke@users.noreply.github.com>
2023-03-30 13:27:04 +0000

    MovingParts-ish

--- Common/src/main/java/dk/sdu/sesem4/common/data/EntityParts/MovingPart.java
+++ Common/src/main/java/dk/sdu/sesem4/common/data/EntityParts/MovingPart.java
@@ -3,8 +3,14 @@ package dk.sdu.sesem4.common.data.EntityParts;
 import dk.sdu.sesem4.common.data.entity.Entity;
 import dk.sdu.sesem4.common.data.gamedata.GameData;
 
+/**
+ *
+ * @author @AnneLærke & Muhammed
+ */
 public class MovingPart implements EntityPart{
+
     private int speed;
+
     public MovingPart(int speed){
         this.speed = speed;
     }
@@ -17,10 +23,10 @@ public class MovingPart implements EntityPart{
         this.speed = speed;
     }
 
-
     @Override
     public void process(GameData gameData, Entity entity) {
-//        PositionPart positionPart = entity.getPart(PositionPart.class);
+        PositionPart positionPart = entity.getPart(PositionPart.class);
+        
 
     }
 }
