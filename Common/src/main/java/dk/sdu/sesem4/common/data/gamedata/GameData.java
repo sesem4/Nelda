9aca04a1acc85b2f214c065654ecbd19bb59cfad
The0mikkel <me@themikkel.dk>
2023-03-30 15:14:04 +0200

    Refactored GameData system

--- Common/src/main/java/dk/sdu/sesem4/common/data/gamedata/GameData.java
+++ Common/src/main/java/dk/sdu/sesem4/common/data/gamedata/GameData.java
@@ -1,4 +1,48 @@
 package dk.sdu.sesem4.common.data.gamedata;
 
+/**
+ * Data for the game
+ */
 public class GameData {
+    /**
+     * Game world data
+     */
+    private GameWorld gameWorld;
+
+    /**
+     * Game data entities
+     */
+    private GameEntities gameEntities;
+    /**
+     * Delta time, which is the time between the last process loop and the current one.
+     */
+    private float deltaTime;
+
+    public GameData() {
+
+    }
+
+    public GameWorld getGameWorld() {
+        return gameWorld;
+    }
+
+    public void setGameWorld(GameWorld gameWorld) {
+        this.gameWorld = gameWorld;
+    }
+
+    public GameEntities getGameEntities() {
+        return gameEntities;
+    }
+
+    public void setGameEntities(GameEntities gameEntities) {
+        this.gameEntities = gameEntities;
+    }
+
+    public float getDeltaTime() {
+        return deltaTime;
+    }
+
+    public void setDeltaTime(float deltaTime) {
+        this.deltaTime = deltaTime;
+    }
 }
