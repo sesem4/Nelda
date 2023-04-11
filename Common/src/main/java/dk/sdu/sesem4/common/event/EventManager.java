8f3631a526d01d8aa392064d80d9d39cc2a9e14d
The0mikkel <me@themikkel.dk>
2023-04-11 08:03:58 +0200

    Fixed docs and typos

--- Common/src/main/java/dk/sdu/sesem4/common/event/EventManager.java
+++ Common/src/main/java/dk/sdu/sesem4/common/event/EventManager.java
@@ -11,7 +11,7 @@ public class EventManager {
      */
     protected static EventManager instance;
     /**
-     * Map of all event and set of listeners
+     * Map of all events and set of listeners
      */
     protected Map<EventType, Set<EventListener>> listeners;
 
@@ -35,11 +35,11 @@ public class EventManager {
     /**
      * Subscribe a listener to a type.
      *
-     * @param type EventType that is to be subscribed to. It may be a subclass.
+     * @param type EventType that is to be subscribed to. It must be a subclass.
      * @param listener EventListener implementation, that should process a notification for the event.
      */
     public void subscribe(EventType type, EventListener listener) {
-        // If the type does not exist in the listernes map, add a new set
+        // If the type does not exist in the listeners map, add a new set
         if (!this.listeners.containsKey(type)) {
             this.listeners.put(type, new LinkedHashSet<>());
         }
@@ -47,18 +47,18 @@ public class EventManager {
         // Get the listener set from the type supplied
         Set<EventListener> listeners = this.listeners.get(type);
 
-        // Add the new listeener to the set
+        // Add the new listener to the set
         listeners.add(listener);
     }
 
     /**
      * Unsubscribe a listener from a type.
      *
-     * @param type EventType that is to be unsubscribed from. It may be a subclass.
+     * @param type EventType that is to be unsubscribed from. It must be a subclass.
      * @param listener EventListener implementation, that is to be unsubscribed from the event.
      */
     public void unsubscribe(EventType type, EventListener listener) {
-        // If the type does not exist in the listernes map, the listener can not be unsubscribed, because it was never subscribed.
+        // If the type does not exist in the listeners map, the listener can not be unsubscribed, because it was never subscribed.
         if (!this.listeners.containsKey(type)) {
             return;
         }
@@ -78,11 +78,11 @@ public class EventManager {
     /**
      * Notify subscribers of an event, with given data.
      *
-     * @param type EventType of the notification, and to which the notification is sent.
-     * @param data Event that is being sent. This object is usually a subclass, which contains datapoints for the event.
+     * @param type EventType of the notification. This will decide which subscribers the notification is sent to.
+     * @param data Event that is being sent. This object is usually a subclass, which contains data-points for the event.
      */
     public void notify(EventType type, Event data) {
-        // If the type does not exist in the listernes map, no EventListener is present to be notified
+        // If the type does not exist in the listeners map, no EventListener is present to be notified
         if (!this.listeners.containsKey(type)) {
             return;
         }
