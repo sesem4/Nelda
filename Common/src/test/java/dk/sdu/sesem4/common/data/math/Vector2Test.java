5d068cce01b4922ea853d205617e80a11ea3973d
AnneLaerke <91538760+AnneLaerke@users.noreply.github.com>
2023-04-17 11:56:59 +0200

    Added Vector2Test

---
+++ Common/src/test/java/dk/sdu/sesem4/common/data/math/Vector2Test.java
@@ -0,0 +1,30 @@
+package dk.sdu.sesem4.common.data.math;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class Vector2Test {
+    Vector2 a;
+    Vector2 b;
+
+    @Test
+    public void testAdd() {
+        a = new Vector2(2, 3);
+        b = new Vector2(5, 1);
+
+        Vector2 actual = a.plus(b);
+
+        Vector2 expected = new Vector2(7, 4);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testAddWorksForNegatives() {
+        a = new Vector2(2, -3);
+        b = new Vector2(-5, 1);
+
+        Vector2 actual = a.plus(b);
+
+        Vector2 expected = new Vector2(-3, -2);
+        assertEquals(expected, actual);
+    }
+}
