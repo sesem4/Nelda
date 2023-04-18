1a10dfc7813084bfb7d4a224239eaba30d9a9918
AnneLaerke <91538760+AnneLaerke@users.noreply.github.com>
2023-04-18 08:54:14 +0200

    Added JavaDocs to KnockbackPart + access modifiers

--- Common/src/main/java/dk/sdu/sesem4/common/data/CollisionParts/KnockBackPart.java
+++ Common/src/main/java/dk/sdu/sesem4/common/data/CollisionParts/KnockBackPart.java
@@ -1,18 +1,32 @@
 package dk.sdu.sesem4.common.data.CollisionParts;
 
 public class KnockbackPart implements CollisionPart {
-	int duration;
-	float speed;
 
+	private int duration;
+	private float speed;
+
+	/**
+	 * Constructs a KnockbackPart
+	 * @param duration for how long the Entity Knockback effects should be applied to another Entity upon collision
+	 * @param speed how fast the Entity Knockback should push another Entity upon collision
+	 */
 	public KnockbackPart(int duration, float speed) {
 		this.duration = duration;
 		this.speed = speed;
 	}
 
+	/**
+	 * Gets for how long the Entity Knockback effects should be applied to another Entity upon collision
+	 * @return duration
+	 */
 	public int getDuration() {
 		return duration;
 	}
 
+	/**
+	 * Gets the speed with which the Entity Knockback should push another Entity upon collision
+	 * @return speed
+	 */
 	public float getSpeed() {
 		return speed;
 	}
