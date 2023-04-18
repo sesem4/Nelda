2aac9dfb27e3ca599fc599251586630274536285
AnneLaerke <91538760+AnneLaerke@users.noreply.github.com>
2023-04-18 08:52:20 +0200

    Added JavaDocs to DamagePart

--- Common/src/main/java/dk/sdu/sesem4/common/data/CollisionParts/DamagePart.java
+++ Common/src/main/java/dk/sdu/sesem4/common/data/CollisionParts/DamagePart.java
@@ -4,10 +4,18 @@ public class DamagePart implements CollisionPart {
 
     private final int damage;
 
+    /**
+     * Constructs a DamagePart
+     * @param damage the amount of damage that should be dealt to another Entity, upon collision
+     */
     public DamagePart(int damage) {
         this.damage = damage;
     }
 
+    /**
+     * Gets the damage value of the DamagePart
+     * @return damage
+     */
     public int getDamage() {
         return damage;
     }
