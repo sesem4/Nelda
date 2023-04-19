134e7c3a7d6679c6e15a618fc2e58948603ef98d
jakob753951 <jlm1999@live.dk>
2023-04-19 11:18:39 +0200

    Removed map and Event system from MapPlugin.java

--- Map/src/main/java/dk/sdu/sesem4/map/MapPlugin.java
+++ Map/src/main/java/dk/sdu/sesem4/map/MapPlugin.java
@@ -4,17 +4,11 @@ import dk.sdu.sesem4.common.SPI.PluginServiceSPI;
 import dk.sdu.sesem4.common.data.gamedata.GameData;
 import dk.sdu.sesem4.common.data.gamedata.GameWorld;
 import dk.sdu.sesem4.common.data.process.Priority;
-import dk.sdu.sesem4.common.event.*;
 
 /**
  * The MapPlugin class is the entry point for the map module.
  */
 public class MapPlugin implements PluginServiceSPI {
-	/**
-	 * The Map object that is used to store the map data.
-	 */
-	private Map map;
-
 	/**
 	 * The MapProcessingService object that is used to process the map data.
 	 */
@@ -28,27 +22,9 @@ public class MapPlugin implements PluginServiceSPI {
 	 */
 	@Override
 	public void start(GameData gameData) {
-		this.map = new Map();
-		this.mapProcessingService = new MapProcessingService(map);
+		this.mapProcessingService = new MapProcessingService();
 		this.mapProcessingService.loadWorld("overworld", 16, 8);
 		gameData.setGameWorld(new GameWorld(mapProcessingService.getCurrentMap()));
-		EventManager.getInstance().subscribe(MapTransitionEventType.class, (eventType, data) -> {
-			System.out.println("Got notified!");
-			switch (((MapTransitionEvent)data).getDirection()) {
-				case UP:
-					map.currentMapIndex -= 16;
-					break;
-				case DOWN:
-					map.currentMapIndex += 16;
-					break;
-				case LEFT:
-					map.currentMapIndex -= 1;
-					break;
-				case RIGHT:
-					map.currentMapIndex += 1;
-					break;
-			}
-		});
 	}
 
 	/**
@@ -59,7 +35,6 @@ public class MapPlugin implements PluginServiceSPI {
 	@Override
 	public void stop(GameData gameData) {
 		this.mapProcessingService = null;
-		this.map = null;
 	}
 
 	/**
