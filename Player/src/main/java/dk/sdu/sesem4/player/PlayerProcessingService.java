2f1127e874859b265ecdf3ad085b6ca3220a8719
JfriisJ <40535086+JfriisJ@users.noreply.github.com>
2023-04-23 08:31:37 +0200

    removed unused import

--- Player/src/main/java/dk/sdu/sesem4/player/PlayerProcessingService.java
+++ Player/src/main/java/dk/sdu/sesem4/player/PlayerProcessingService.java
@@ -2,18 +2,14 @@ package dk.sdu.sesem4.player;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import dk.sdu.sesem4.common.SPI.PostProcessingServiceSPI;
 import dk.sdu.sesem4.common.SPI.ProcessingServiceSPI;
 import dk.sdu.sesem4.common.data.entity.Entity;
 import dk.sdu.sesem4.common.data.entity.EntityType;
 import dk.sdu.sesem4.common.data.gamedata.GameData;
 import dk.sdu.sesem4.common.data.process.Priority;
-import dk.sdu.sesem4.common.event.EventManager;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 public class PlayerProcessingService implements ProcessingServiceSPI, PostProcessingServiceSPI {
 
