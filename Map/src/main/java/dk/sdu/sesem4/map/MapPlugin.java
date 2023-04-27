61ae93bdf6ac2910f4719c87a88763236f0a9767
Michael Gertz <michael@ringhus.dk>
2023-04-27 22:30:46 +0200

    added formatting
    Co-Authored-By: JfriisJ <40535086+JfriisJ@users.noreply.github.com>

--- Map/src/main/java/dk/sdu/sesem4/map/MapPlugin.java
+++ Map/src/main/java/dk/sdu/sesem4/map/MapPlugin.java
@@ -7,7 +7,7 @@ import dk.sdu.sesem4.common.data.gamedata.GameWorld;
 /**
  * The MapPlugin class is the entry point for the map module.
  */
-public class MapPlugin implements PluginServiceSPI{
+public class MapPlugin implements PluginServiceSPI {
 
 	/**
 	 * The MapProcessingService object that is used to process the map data.
@@ -18,6 +18,7 @@ public class MapPlugin implements PluginServiceSPI{
 	 * This method is called when the game is started.
 	 * It creates a new Map object, a new MapProcessingService object and loads the world.
 	 * It also subscribes to the MapTransitionEventType.
+	 *
 	 * @param gameData The game data.
 	 */
 	@Override
@@ -29,6 +30,7 @@ public class MapPlugin implements PluginServiceSPI{
 	/**
 	 * This method is called when the game is stopped.
 	 * It sets the MapProcessingService and the Map object to null.
+	 *
 	 * @param gameData The game data.
 	 */
 	@Override
