bda2cfbe851993e99b7184bf11410570ded36de0
JfriisJ <40535086+JfriisJ@users.noreply.github.com>
2023-04-27 21:30:26 +0200

    removed unused processing services in MapPlugin, and added postprocessingservice to MapProcessingService

--- Map/src/main/java/dk/sdu/sesem4/map/MapPlugin.java
+++ Map/src/main/java/dk/sdu/sesem4/map/MapPlugin.java
@@ -10,7 +10,7 @@ import dk.sdu.sesem4.common.data.process.Priority;
 /**
  * The MapPlugin class is the entry point for the map module.
  */
-public class MapPlugin implements PluginServiceSPI, ProcessingServiceSPI, PostProcessingServiceSPI {
+public class MapPlugin implements PluginServiceSPI{
 
 	/**
 	 * The MapProcessingService object that is used to process the map data.
@@ -25,8 +25,6 @@ public class MapPlugin implements PluginServiceSPI, ProcessingServiceSPI, PostPr
 	 */
 	@Override
 	public void start(GameData gameData) {
-		System.out.println("Vi har nu startet map!");
-
 		this.mapProcessingService = new MapProcessingService();
 		gameData.setGameWorld(new GameWorld(mapProcessingService.getCurrentMap()));
 	}
@@ -39,28 +37,5 @@ public class MapPlugin implements PluginServiceSPI, ProcessingServiceSPI, PostPr
 	@Override
 	public void stop(GameData gameData) {
 		this.mapProcessingService = null;
-	}
-
-
-	/**
-	 * This method is called after the process method.
-	 * @param gameData The game data
-	 * @param priority The priority, which is to be run for the current process round
-	 */
-	@Override
-	public void postProcess(GameData gameData, Priority priority) {
-		this.mapProcessingService.postProcess(gameData, priority);
-	}
-
-	/**
-	 * This method sets the current map in the game data. If there is a MapTransition event,
-	 * it changes the current map index to reflect that.
-	 *
-	 * @param gameData The game data.
-	 * @param priority The priority of the process.
-	 */
-	@Override
-	public void process(GameData gameData, Priority priority) {
-		this.mapProcessingService.process(gameData, priority);
 	}
 }
