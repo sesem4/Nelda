d9936fcaa1f6a5cf2206dfed2c05eb38385bba05
jakob753951 <jlm1999@live.dk>
2023-04-27 14:47:09 +0200

    Reindented with Tabs instead of spaces

--- Common/src/main/java/dk/sdu/sesem4/common/SPI/MovementControllerSPI.java
+++ Common/src/main/java/dk/sdu/sesem4/common/SPI/MovementControllerSPI.java
@@ -5,13 +5,13 @@ import dk.sdu.sesem4.common.data.gamedata.GameData;
 import dk.sdu.sesem4.common.util.Direction;
 
 public interface MovementControllerSPI {
-    /**
-     * Get desired movement
-     *
-     * @param gameData Current gameData for game.
-     * @param entity Entity of which wish to get movement direction.
-     * @return Direction of which the entity should move. This can be Null, if the entity should not move.
-     */
-    Direction getMovement(GameData gameData, Entity entity);
-
+	/**
+	 * Get desired movement
+	 *
+	 * @param gameData Current gameData for game.
+	 * @param entity Entity of which wish to get movement direction.
+	 * @return Direction of which the entity should move. This can be Null, if the entity should not move.
+	 */
+	Direction getMovement(GameData gameData, Entity entity);
+	
 }
--- Common/src/main/java/dk/sdu/sesem4/common/event/EventManager.java
+++ Common/src/main/java/dk/sdu/sesem4/common/event/EventManager.java
@@ -14,91 +14,91 @@ import java.util.Set;
  * For further information on how to use, such as example, see tests
  */
 public class EventManager {
-    /**
-     * EventManger instance
-     */
-    protected static EventManager instance;
-    /**
-     * Map of all events and set of listeners
-     */
-    protected Map<Class<? extends EventType>, Set<EventListener>> listeners;
-
-    private EventManager() {
-        this.listeners = new HashMap<>();
-    }
-
-    /**
-     * Get instance for the event manager.
-     *
-     * @return EventManager
-     */
-    public static EventManager getInstance() {
-        if (EventManager.instance == null) {
-            EventManager.instance = new EventManager();
-        }
-
-        return EventManager.instance;
-    }
-
-    /**
-     * Subscribe a listener to a type.
-     *
-     * @param type EventType class that is to be subscribed to. It must be a subclass.
-     * @param listener EventListener implementation, that should process a notification for the event.
-     */
-    public void subscribe(Class<? extends EventType> type, EventListener listener) {
-        // If the type does not exist in the listeners map, add a new set
-        if (!this.listeners.containsKey(type)) {
-            this.listeners.put(type, new LinkedHashSet<>());
-        }
-
-        // Get the listener set from the type supplied
-        Set<EventListener> listeners = this.listeners.get(type);
-
-        // Add the new listener to the set
-        listeners.add(listener);
-    }
-
-    /**
-     * Unsubscribe a listener from a type.
-     *
-     * @param type EventType class that is to be unsubscribed from. It must be a subclass.
-     * @param listener EventListener implementation, that is to be unsubscribed from the event.
-     */
-    public void unsubscribe(Class<? extends EventType> type, EventListener listener) {
-        // If the type does not exist in the listeners map, the listener can not be unsubscribed, because it was never subscribed.
-        if (!this.listeners.containsKey(type)) {
-            return;
-        }
-
-        // Get the listener set from the type supplied
-        Set<EventListener> listeners = this.listeners.get(type);
-
-        // Remove listener from the listeners for the type
-        listeners.remove(listener);
-
-        // if the listeners list is empty, remove it completely from the listeners map
-        if (listeners.isEmpty()) {
-            this.listeners.remove(type);
-        }
-    }
-
-    /**
-     * Notify subscribers of an event, with given data.
-     *
-     * @param type EventType class of the notification. This will decide which subscribers the notification is sent to.
-     * @param data Event that is being sent. This object is usually a subclass, which contains data-points for the event.
-     */
-    public void notify(Class<? extends EventType> type, Event data) {
-        // If the type does not exist in the listeners map, no EventListener is present to be notified
-        if (!this.listeners.containsKey(type)) {
-            return;
-        }
-
-        // Get the listener set from the type supplied
-        Set<EventListener> listeners = this.listeners.get(type);
-
-        // For each listener, call the processNotification method
-        listeners.forEach(listener -> listener.processNotification(type, data));
-    }
+	/**
+	 * EventManger instance
+	 */
+	protected static EventManager instance;
+	/**
+	 * Map of all events and set of listeners
+	 */
+	protected Map<Class<? extends EventType>, Set<EventListener>> listeners;
+	
+	private EventManager() {
+		this.listeners = new HashMap<>();
+	}
+	
+	/**
+	 * Get instance for the event manager.
+	 *
+	 * @return EventManager
+	 */
+	public static EventManager getInstance() {
+		if (EventManager.instance == null) {
+			EventManager.instance = new EventManager();
+		}
+		
+		return EventManager.instance;
+	}
+	
+	/**
+	 * Subscribe a listener to a type.
+	 *
+	 * @param type EventType class that is to be subscribed to. It must be a subclass.
+	 * @param listener EventListener implementation, that should process a notification for the event.
+	 */
+	public void subscribe(Class<? extends EventType> type, EventListener listener) {
+		// If the type does not exist in the listeners map, add a new set
+		if (!this.listeners.containsKey(type)) {
+			this.listeners.put(type, new LinkedHashSet<>());
+		}
+		
+		// Get the listener set from the type supplied
+		Set<EventListener> listeners = this.listeners.get(type);
+		
+		// Add the new listener to the set
+		listeners.add(listener);
+	}
+	
+	/**
+	 * Unsubscribe a listener from a type.
+	 *
+	 * @param type EventType class that is to be unsubscribed from. It must be a subclass.
+	 * @param listener EventListener implementation, that is to be unsubscribed from the event.
+	 */
+	public void unsubscribe(Class<? extends EventType> type, EventListener listener) {
+		// If the type does not exist in the listeners map, the listener can not be unsubscribed, because it was never subscribed.
+		if (!this.listeners.containsKey(type)) {
+			return;
+		}
+		
+		// Get the listener set from the type supplied
+		Set<EventListener> listeners = this.listeners.get(type);
+		
+		// Remove listener from the listeners for the type
+		listeners.remove(listener);
+		
+		// if the listeners list is empty, remove it completely from the listeners map
+		if (listeners.isEmpty()) {
+			this.listeners.remove(type);
+		}
+	}
+	
+	/**
+	 * Notify subscribers of an event, with given data.
+	 *
+	 * @param type EventType class of the notification. This will decide which subscribers the notification is sent to.
+	 * @param data Event that is being sent. This object is usually a subclass, which contains data-points for the event.
+	 */
+	public void notify(Class<? extends EventType> type, Event data) {
+		// If the type does not exist in the listeners map, no EventListener is present to be notified
+		if (!this.listeners.containsKey(type)) {
+			return;
+		}
+		
+		// Get the listener set from the type supplied
+		Set<EventListener> listeners = this.listeners.get(type);
+		
+		// For each listener, call the processNotification method
+		listeners.forEach(listener -> listener.processNotification(type, data));
+	}
 }
