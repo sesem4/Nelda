b85081ef604ecb495e889188b55f0e1af507c2d5
Mikkel Albrechtsen <mikkel.alb@gmail.com>
2023-05-02 15:55:53 +0200

    Added sprite id

--- Common/src/main/java/dk/sdu/sesem4/common/data/EntityParts/SpritePart.java
+++ Common/src/main/java/dk/sdu/sesem4/common/data/EntityParts/SpritePart.java
@@ -4,9 +4,14 @@ import dk.sdu.sesem4.common.data.entity.Entity;
 import dk.sdu.sesem4.common.data.gamedata.GameData;
 import dk.sdu.sesem4.common.data.rendering.SpriteData;
 
+import java.util.UUID;
+
 public class SpritePart implements EntityPart {
 	/** Current sprite for entity */
 	private SpriteData sprite;
+
+	/** Id */
+	private final UUID id;
 
 	/**
 	 * Construct SpritePart with sprite
@@ -15,6 +20,7 @@ public class SpritePart implements EntityPart {
 	 */
 	public SpritePart(SpriteData sprite) {
 		this.sprite = sprite;
+		this.id = UUID.randomUUID();
 	}
 
 	public SpriteData getSprite() {
@@ -23,6 +29,10 @@ public class SpritePart implements EntityPart {
 
 	public void setSprite(SpriteData sprite) {
 		this.sprite = sprite;
+	}
+
+	public UUID getId() {
+		return id;
 	}
 
 	@Override
