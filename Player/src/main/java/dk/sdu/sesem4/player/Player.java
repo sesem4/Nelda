7e5d929bd0df5f096cad9191cac2757651e615e0
Michael Gertz <michael@ringhus.dk>
2023-05-07 22:32:11 +0200

    Clean up

--- Player/src/main/java/dk/sdu/sesem4/player/Player.java
+++ Player/src/main/java/dk/sdu/sesem4/player/Player.java
@@ -2,8 +2,6 @@ package dk.sdu.sesem4.player;
 
 import dk.sdu.sesem4.common.data.entity.Entity;
 import dk.sdu.sesem4.common.data.entity.EntityType;
-import dk.sdu.sesem4.common.data.math.Vector2;
-import dk.sdu.sesem4.common.util.Direction;
 
 public class Player extends Entity {
 
