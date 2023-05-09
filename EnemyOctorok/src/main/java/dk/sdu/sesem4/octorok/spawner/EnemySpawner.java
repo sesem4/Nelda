c98d1d2dc6a0a9797c203b3c64439ba304b7a2ab
Michael Gertz <michael@ringhus.dk>
2023-05-09 15:30:32 +0200

    Enabled spawnEnemy()
    Co-Authored-By: AnneLaerke <91538760+AnneLaerke@users.noreply.github.com>

--- EnemyOctorok/src/main/java/dk/sdu/sesem4/octorok/spawner/EnemySpawner.java
+++ EnemyOctorok/src/main/java/dk/sdu/sesem4/octorok/spawner/EnemySpawner.java
@@ -1,6 +1,8 @@
 package dk.sdu.sesem4.octorok.spawner;
 
-import dk.sdu.sesem4.common.SPI.EnemySpawnerSPI;
+//import dk.sdu.sesem4.common.SPI.EnemySpawnerSPI;
+
+import dk.sdu.sesem4.common.SPI.SpawnableEnemySPI;
 import dk.sdu.sesem4.common.data.gamedata.GameData;
 import dk.sdu.sesem4.common.data.math.Vector2;
 import dk.sdu.sesem4.octorok.blue.BlueOctorok;
@@ -8,36 +10,30 @@ import dk.sdu.sesem4.octorok.blue.BlueOctorokPlugin;
 import dk.sdu.sesem4.octorok.red.RedOctorok;
 import dk.sdu.sesem4.octorok.red.RedOctorokPlugin;
 
-public class EnemySpawner implements EnemySpawnerSPI {
+public class EnemySpawner implements SpawnableEnemySPI {
 
 	@Override
-	public void spawnEnemy(GameData gameData, float x, float y) {
-		throw new UnsupportedOperationException("Unimplemented method 'spawnEnemy' - Is to be replaced");
+	public void spawnEnemy(GameData gameData, Vector2 coordinate) {
+		// Call the overload method with a difficulty (red Octorok)
+		spawnEnemy(gameData, coordinate, 1);
 	}
 
+	@Override
+	public void spawnEnemy(GameData gameData, Vector2 coordinate, int difficulty) {
+		switch (difficulty) {
+			case 1: // red
+				RedOctorokPlugin redOctorokPlugin = new RedOctorokPlugin();
+				redOctorokPlugin.spawn(gameData, coordinate, RedOctorok.class);
+				break;
+			case 2: // blue
+				BlueOctorokPlugin blueOctorokPlugin = new BlueOctorokPlugin();
+				blueOctorokPlugin.spawn(gameData, coordinate, BlueOctorok.class);
+				break;
+		}
+	}
 
-	// @Override
-	// public void spawnEnemy(GameData gameData, Vector2 coordinate) {
-	// 	// Call the overload method with a difficulty (red Octorok)
-	// 	spawnEnemy(gameData, coordinate, 1);
-	// }
-
-	// @Override
-	// public void spawnEnemy(GameData gameData, Vector2 coordinate, int difficulty) {
-	// 	switch (difficulty) {
-	// 		case 1: // red
-	// 			RedOctorokPlugin redOctorokPlugin = new RedOctorokPlugin();
-	// 			redOctorokPlugin.spawn(gameData, coordinate, RedOctorok.class);
-	// 			break;
-	// 		case 2: // blue
-	// 			BlueOctorokPlugin blueOctorokPlugin = new BlueOctorokPlugin();
-	// 			blueOctorokPlugin.spawn(gameData, coordinate, BlueOctorok.class);
-	// 			break;
-	// 	}
-	// }
-
-	// @Override
-	// public int[] getDifficulties() {
-	// 	return new int[]{1, 2};
-	// }
+	@Override
+	public int[] getDifficulties() {
+		return new int[]{1, 2};
+	}
 }
