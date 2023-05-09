d54da240d8556ff8a0349e453c342b7fa9075b09
Michael Gertz <michael@ringhus.dk>
2023-05-09 15:29:27 +0200

    provide PostProcessor
    Co-Authored-By: AnneLaerke <91538760+AnneLaerke@users.noreply.github.com>

--- EnemyOctorok/src/main/java/module-info.java
+++ EnemyOctorok/src/main/java/module-info.java
@@ -1,15 +1,18 @@
 import dk.sdu.sesem4.common.SPI.PluginServiceSPI;
+import dk.sdu.sesem4.common.SPI.PostProcessingServiceSPI;
 import dk.sdu.sesem4.common.SPI.ProcessingServiceSPI;
 
 module Octorok {
 	requires Common;
 
 	provides PluginServiceSPI with
-			dk.sdu.sesem4.octorok.red.RedOctorokPlugin,
-			dk.sdu.sesem4.octorok.blue.BlueOctorokPlugin;
+		dk.sdu.sesem4.octorok.red.RedOctorokPlugin,
+		dk.sdu.sesem4.octorok.blue.BlueOctorokPlugin;
 	provides ProcessingServiceSPI with
-			dk.sdu.sesem4.octorok.red.RedOctorokProcessor,
-			dk.sdu.sesem4.octorok.blue.BlueOctorokProcessor;
+		dk.sdu.sesem4.octorok.red.RedOctorokProcessor,
+		dk.sdu.sesem4.octorok.blue.BlueOctorokProcessor;
+	provides PostProcessingServiceSPI with
+		dk.sdu.sesem4.octorok.common.OctorokPostProcessor;
 
 	exports dk.sdu.sesem4.octorok.blue;
 	exports dk.sdu.sesem4.octorok.red;
