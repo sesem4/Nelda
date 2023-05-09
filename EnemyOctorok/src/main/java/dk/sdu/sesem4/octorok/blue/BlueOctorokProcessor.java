2ccdfe043f48c3f5dfa259381a8a8440100cc689
Michael Gertz <michael@ringhus.dk>
2023-05-09 17:09:21 +0200

    deleted as not used
    Co-Authored-By: AnneLaerke <91538760+AnneLaerke@users.noreply.github.com>

--- EnemyOctorok/src/main/java/dk/sdu/sesem4/octorok/blue/BlueOctorokProcessor.java
+++
@@ -1,6 +0,0 @@
-package dk.sdu.sesem4.octorok.blue;
-
-import dk.sdu.sesem4.octorok.common.OctorokProcessor;
-
-public class BlueOctorokProcessor extends OctorokProcessor {
-}
--- EnemyOctorok/src/main/java/dk/sdu/sesem4/octorok/red/RedOctorokProcessor.java
+++
@@ -1,6 +0,0 @@
-package dk.sdu.sesem4.octorok.red;
-
-import dk.sdu.sesem4.octorok.common.OctorokProcessor;
-
-public class RedOctorokProcessor extends OctorokProcessor {
-}
