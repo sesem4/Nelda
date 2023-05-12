313b6667ef467c0bed885fd18c547b1de1dee333
Michael Gertz <michael@ringhus.dk>
2023-05-12 07:47:16 +0200

    Added CombatControllerSPI

--- Common/src/main/java/dk/sdu/sesem4/common/SPI/CombatControllerSPI.java
+++ Common/src/main/java/dk/sdu/sesem4/common/SPI/CombatControllerSPI.java
@@ -1,4 +1,32 @@
 package dk.sdu.sesem4.common.SPI;
 
+import dk.sdu.sesem4.common.data.combat.Attack;
+import dk.sdu.sesem4.common.data.entity.Entity;
+import dk.sdu.sesem4.common.data.gamedata.GameData;
+
 public interface CombatControllerSPI {
+
+	/**
+	 * Checks if the entity should attack
+	 * <p>
+	 * Pre-condition: The game has been started, and the SPI has been implemented in a class.
+	 * post-condition: Returns if a combat should be initiated
+	 *
+	 * @param gameData The game data
+	 * @param entity   The entity which is requesting combat
+	 * @return the AttackType
+	 */
+	boolean shouldAttack(GameData gameData, Entity entity);
+
+	/**
+	 * Spawn an attack from the provided entity
+	 * <p>
+	 * Pre-condition: The game has been started, and the SPI has been implemented in a class.
+	 * post-condition: Returns an Attack and spawns an attack in the world.
+	 *
+	 * @param gameData The game data
+	 * @param entity   The entity which is requesting combat
+	 * @return the AttackType
+	 */
+	Attack spawnAttack(GameData gameData, Entity entity);
 }
