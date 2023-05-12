27c84742df7926d4846108db1b2198fa6eccc569
Michael Gertz <michael@ringhus.dk>
2023-05-12 09:49:36 +0200

    Added JavaDoc

--- Common/src/main/java/dk/sdu/sesem4/common/data/combat/Weapon.java
+++ Common/src/main/java/dk/sdu/sesem4/common/data/combat/Weapon.java
@@ -2,12 +2,18 @@ package dk.sdu.sesem4.common.data.combat;
 
 import dk.sdu.sesem4.common.data.entity.Entity;
 
+/**
+ * Used to determine the duration of a weapon
+ */
 public class Weapon {
 
+	// Duration of the weapon
 	private float duration;
 
+	// Life time of the weapon
 	private final float lifeTime;
 
+	// The entity that is using the weapon
 	private final Entity entity;
 
 	public Weapon(float lifeTime, Entity entity) {
@@ -15,22 +21,47 @@ public class Weapon {
 		this.entity = entity;
 	}
 
+	/**
+	 * Get the duration of the weapon
+	 *
+	 * @return The duration of the weapon
+	 */
 	public float getDuration() {
 		return duration;
 	}
 
+	/**
+	 * Get the life time of the weapon
+	 *
+	 * @return The life time of the weapon
+	 */
 	public float getLifeTime() {
 		return lifeTime;
 	}
 
+	/**
+	 * Increment the duration of the weapon
+	 *
+	 * @param deltaTime The time since the last update
+	 */
 	public void incrementDuration(float deltaTime) {
 		this.duration += deltaTime;
 	}
 
+	/**
+	 * Check if the weapon is finished
+	 *
+	 * @return True if the weapon is finished
+	 */
 	public boolean isFinished() {
 		return this.duration >= this.lifeTime;
 	}
 
+	/**
+	 * Get the entity that is using the weapon
+	 *
+	 * @return The entity of the weapon
+	 */
 	public Entity getEntity() {
 		return entity;
 	}
--- Common/src/main/java/dk/sdu/sesem4/common/data/combat/WeaponType.java
+++ Common/src/main/java/dk/sdu/sesem4/common/data/combat/WeaponType.java
@@ -1,5 +1,8 @@
 package dk.sdu.sesem4.common.data.combat;
 
+/**
+ * Used to determine which weapon type is use
+ */
 public enum WeaponType {
 	SWORD,
 	ROCK,
