71bd0d33327a0c365c369006323798863fc9bec9
Michael Gertz <michael@ringhus.dk>
2023-05-12 12:18:26 +0200

    added file

---
+++ EnemyOctorok/src/test/java/dk/sdu/sesem4/octorok/blue/BlueOctorokPluginTest.java
@@ -0,0 +1,30 @@
+package dk.sdu.sesem4.octorok.blue;
+
+import dk.sdu.sesem4.common.data.CollisionParts.CollisionPart;
+import dk.sdu.sesem4.common.data.EntityParts.LifePart;
+import dk.sdu.sesem4.common.data.entity.Entity;
+import dk.sdu.sesem4.common.data.gamedata.GameData;
+import dk.sdu.sesem4.common.data.gamedata.GameEntities;
+import dk.sdu.sesem4.common.data.math.Vector2;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class BlueOctorokPluginTest {
+
+	@BeforeEach
+	void setUp() {
+
+	}
+
+	@AfterEach
+	void tearDown() {
+	}
+
+	@Test
+	void spawn() {
+	}
+}
---
+++ EnemyOctorok/src/test/java/dk/sdu/sesem4/octorok/red/RedOctorokPluginTest.java
@@ -0,0 +1,24 @@
+package dk.sdu.sesem4.octorok.red;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RedOctorokPluginTest {
+
+	@BeforeEach
+	void setUp() {
+	}
+
+	@AfterEach
+	void tearDown() {
+	}
+
+
+	@Test
+	void spawn() {
+
+	}
+}
