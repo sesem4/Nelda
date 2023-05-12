1ca4a095a906f8c6762a6a60697d323fa6ddd192
Mikkel Albrechtsen <mikkel.alb@gmail.com>
2023-05-12 09:24:10 +0200

    Added spi implementation and export

--- EnemyOctorok/src/main/java/module-info.java
+++ EnemyOctorok/src/main/java/module-info.java
@@ -1,17 +1,22 @@
 import dk.sdu.sesem4.common.SPI.PluginServiceSPI;
 import dk.sdu.sesem4.common.SPI.PostProcessingServiceSPI;
 import dk.sdu.sesem4.common.SPI.ProcessingServiceSPI;
+import dk.sdu.sesem4.common.SPI.SpawnableEnemySPI;
+import dk.sdu.sesem4.octorok.blue.BlueOctorokPlugin;
+import dk.sdu.sesem4.octorok.common.OctorokPostProcessor;
+import dk.sdu.sesem4.octorok.common.OctorokProcessor;
+import dk.sdu.sesem4.octorok.red.RedOctorokPlugin;
+import dk.sdu.sesem4.octorok.spawner.EnemySpawner;
 
 module Octorok {
 	requires Common;
 
 	provides PluginServiceSPI with
-		dk.sdu.sesem4.octorok.red.RedOctorokPlugin,
-		dk.sdu.sesem4.octorok.blue.BlueOctorokPlugin;
-	provides ProcessingServiceSPI with
-		dk.sdu.sesem4.octorok.common.OctorokProcessor;
-	provides PostProcessingServiceSPI with
-		dk.sdu.sesem4.octorok.common.OctorokPostProcessor;
+			RedOctorokPlugin,
+			BlueOctorokPlugin;
+	provides ProcessingServiceSPI with OctorokProcessor;
+	provides PostProcessingServiceSPI with OctorokPostProcessor;
+	provides SpawnableEnemySPI with EnemySpawner;
 
 	exports dk.sdu.sesem4.octorok.blue;
 	exports dk.sdu.sesem4.octorok.red;
