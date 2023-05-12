b86f0424074aa4a92d0e87d9b8906188f6361c48
Michael Gertz <michael@ringhus.dk>
2023-05-12 09:51:41 +0200

    Update CombatPart.java

--- Common/src/main/java/dk/sdu/sesem4/common/data/EntityParts/CombatPart.java
+++ Common/src/main/java/dk/sdu/sesem4/common/data/EntityParts/CombatPart.java
@@ -11,6 +11,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * A class that represents the CombatPart.
+ */
 public class CombatPart implements EntityPart {
 
 	/**
