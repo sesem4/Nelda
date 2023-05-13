2e857947748317ecf74110fc99b4eb6ff9ada2fc
JfriisJ <40535086+JfriisJ@users.noreply.github.com>
2023-05-13 08:44:46 +0200

    added SmartAI based on SPILocator added a implementation of weighted A*

---
+++ SmartAI/src/main/java/dk/sdu/sesem4/smartAIMovement/SmartAIMovement.java
@@ -0,0 +1,25 @@
+package dk.sdu.sesem4.smartAIMovement;
+
+import dk.sdu.sesem4.common.SPI.ControlSPI;
+import dk.sdu.sesem4.common.SPI.MovementControllerSPI;
+import dk.sdu.sesem4.common.data.controllerParts.ControlType;
+
+public class SmartAIMovement implements ControlSPI {
+    private ControlType controlType;
+    private SmartAIMovementController smartAIMovementController;
+
+    public SmartAIMovement(){
+        this.controlType = ControlType.MEDIUM_AI;
+        this.smartAIMovementController = new SmartAIMovementController();
+    }
+
+    @Override
+    public ControlType getType() {
+        return this.controlType;
+    }
+
+    @Override
+    public MovementControllerSPI getMovementController() {
+        return this.smartAIMovementController;
+    }
+}
