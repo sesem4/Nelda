dae93d18b53733ba59ccff0562ff43173ef949dd
jakob753951 <jlm1999@live.dk>
2023-05-18 01:02:57 +0200

    Yes, technically breaking changes. No, we don't care.

--- Map/src/main/java/dk/sdu/sesem4/map/MapPostProcessingService.java
+++ Map/src/main/java/dk/sdu/sesem4/map/MapPostProcessingService.java
@@ -18,15 +18,14 @@ public class MapPostProcessingService implements PostProcessingServiceSPI {
 	 */
 	@Override
 	public void postProcess(GameData gameData, Priority priority) {
-		checkMapCollisions(gameData);
+		checkMapCollisions(gameData, Map.getInstance());
 	}
 
 	/**
 	 * Determines if a given entity can move on the map.
 	 * @param gameData The game data.
 	 */
-	protected void checkMapCollisions(GameData gameData) {
-		Map map = Map.getInstance();
+	protected void checkMapCollisions(GameData gameData, Map map) {
 		MapUtil mapUtil = new MapUtil();
 		TiledMap currentTiledMap = map.getCurrentTiledMap();
 		for (Entity entity : gameData.getGameEntities().getEntities()) {
