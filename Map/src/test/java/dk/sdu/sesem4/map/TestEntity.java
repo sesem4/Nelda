e2671f53b8489433cb89e0caa4474ded2de519fc
jakob753951 <jlm1999@live.dk>
2023-05-18 01:04:18 +0200

    Added MapPostProcessingServiceTest and TestEntity

---
+++ Map/src/test/java/dk/sdu/sesem4/map/TestEntity.java
@@ -0,0 +1,10 @@
+package dk.sdu.sesem4.map;
+
+import dk.sdu.sesem4.common.data.entity.Entity;
+import dk.sdu.sesem4.common.data.entity.EntityType;
+
+public class TestEntity extends Entity {
+	public TestEntity(EntityType entityType) {
+		super(entityType);
+	}
+}
