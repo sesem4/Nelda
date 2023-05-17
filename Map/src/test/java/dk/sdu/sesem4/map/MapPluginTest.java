5017d9a8b5b75a5008252c81d4c26076906658a9
jakob753951 <jlm1999@live.dk>
2023-05-17 22:21:12 +0200

    Added MapUtilTest and MapPluginTest to replace MapProcessingServiceTest.java

--- Map/src/test/java/dk/sdu/sesem4/map/MapPluginTest.java
+++ Map/src/test/java/dk/sdu/sesem4/map/MapPluginTest.java
@@ -1,4 +1,31 @@
 package dk.sdu.sesem4.map;
 
-class MapPluginTest {
+import dk.sdu.sesem4.common.data.gamedata.GameData;
+import dk.sdu.sesem4.common.data.gamedata.GameWorld;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MapPluginTest {
+	MapPlugin mapPlugin;
+	
+	@Before
+	public void setup() {
+		mapPlugin = new MapPlugin();
+	}
+	
+	
+	@Test
+	public void testStart() {
+		GameData gameData = new GameData();
+		GameWorld before = gameData.getGameWorld();
+		
+		mapPlugin.start(gameData);
+		
+		GameWorld after = gameData.getGameWorld();
+		
+		assertNotEquals(before.getMap(), after.getMap());
+		assertNotEquals(before.getMapSize(), after.getMapSize());
+	}
 }
