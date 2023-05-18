1b77577d12fe54da403f8d6d5c0771d543c9ef63
jakob753951 <jlm1999@live.dk>
2023-05-18 17:56:25 +0200

    Made SmartAI follow Player

--- SmartAI/src/main/java/dk/sdu/sesem4/smartAIMovement/SmartAIMovementController.java
+++ SmartAI/src/main/java/dk/sdu/sesem4/smartAIMovement/SmartAIMovementController.java
@@ -4,7 +4,9 @@ import dk.sdu.sesem4.common.SPI.MapSPI;
 import dk.sdu.sesem4.common.SPI.MovementControllerSPI;
 import dk.sdu.sesem4.common.data.EntityParts.PositionPart;
 import dk.sdu.sesem4.common.data.entity.Entity;
+import dk.sdu.sesem4.common.data.entity.EntityType;
 import dk.sdu.sesem4.common.data.gamedata.GameData;
+import dk.sdu.sesem4.common.data.gamedata.GameWorld;
 import dk.sdu.sesem4.common.data.math.Vector2;
 import dk.sdu.sesem4.common.util.Direction;
 import dk.sdu.sesem4.common.util.SPILocator;
@@ -16,9 +18,59 @@ import java.util.*;
  * and provides a simple AI movement algorithm for entities in a game.
  */
 public class SmartAIMovementController implements MovementControllerSPI {
-	private Vector2 randomGoal;
+	private Vector2 goal;
 	
 	public static boolean[][] navGrid;
+	
+	private final TileMover tileMover = new TileMover();
+	
+	private MapSPI getMapSPI() throws SpiNotFoundException {
+		MapSPI mapSPI = SPILocator.locateAll(MapSPI.class).get(0);
+		if (mapSPI == null) {
+			throw new SpiNotFoundException("MapSPI not found.");
+		}
+		return mapSPI;
+	}
+	
+	private void updateNavGrid(GameData gameData) throws SpiNotFoundException {
+		navGrid = getMapSPI().getNavGrid(gameData);
+	}
+	
+	private Vector2 getGoal(GameData gameData) throws PlayerException, SpiNotFoundException {
+		List<Entity> entities = gameData.getGameEntities().getEntities();
+		Entity player = null;
+		for (Entity entity : entities) {
+			if (entity.getEntityType() == EntityType.Player) {
+				player = entity;
+				break;
+			}
+		}
+		if (player == null) {
+			throw new PlayerException("Player not found.");
+		}
+		PositionPart positionPart = player.getEntityPart(PositionPart.class);
+		if (positionPart == null) {
+			throw new PlayerException("No PositionPart on player.");
+		}
+		Vector2 playerPosition = positionPart.getPosition();
+		
+		Vector2 playerTile = playerPosition.divide(GameWorld.TILE_SIZE);
+		
+		if (playerTile.getX() < 0 || playerTile.getY() < 0) {
+			throw new PlayerException("Player is outside map.");
+		}
+		Vector2 mapSize = gameData.getGameWorld().getMapSize().divide(GameWorld.TILE_SIZE);
+		if (playerTile.getX() > mapSize.getX() || playerTile.getY() > mapSize.getY()) {
+			throw new PlayerException("Player is outside map.");
+		}
+		
+		MapSPI mapSPI = getMapSPI();
+		if (!mapSPI.isTilePassable(playerTile)) {
+			throw new PlayerException("Player is not on passable tile.");
+		}
+		
+		return playerTile;
+	}
 	
 	@Override
 	public Direction getMovement(GameData gameData, Entity entity) {
@@ -27,38 +79,53 @@ public class SmartAIMovementController implements MovementControllerSPI {
 		
 		// Get the start position of the entity
 		Vector2 startPosition = positionPart.getPosition();
+		tileMover.position = startPosition;
+		if (tileMover.target == null) {
+			tileMover.target = startPosition;
+		}
 		
-		// Get the current region
-		MapSPI mapSPI = SPILocator.locateAll(MapSPI.class).get(0);
-		if (mapSPI == null) {
-			System.out.println("MapSPI not found.");
+		try {
+			updateNavGrid(gameData);
+		} catch (SpiNotFoundException e) {
 			return null;
 		}
-		navGrid = mapSPI.getNavGrid(gameData);
-//		System.out.println("Printing navGrid:");
-//		for (int y = navGrid[0].length - 1; y >= 0; y--) {
-//			for (int x = 0; x < navGrid.length; x++) {
-//				System.out.print(navGrid[x][y] ? "." : "#");
-//			}
-//			System.out.println();
-//		}
-		if (randomGoal == null) {
-			this.randomGoal = mapSPI.getRandomPassableTile(gameData);
+		
+		if (!tileMover.isDone()) {
+			return tileMover.getDirection();
 		}
 		
 		int xStart = (int) startPosition.getX() / 16;
 		int yStart = (int) startPosition.getY() / 16;
 		
-		int xGoal = (int) randomGoal.getX();
-		int yGoal = (int) randomGoal.getY();
+		if (goal == null) {
+			try {
+				goal = getGoal(gameData);
+			} catch (PlayerException | SpiNotFoundException e) {
+				return null;
+			}
+		}
+		int xGoal = (int) goal.getX();
+		int yGoal = (int) goal.getY();
 		
-		
-		if (xStart == xGoal && yStart == yGoal) {
-			this.randomGoal = null;
+		Vector2 newTarget;
+		try {
+			newTarget = weightedAStar(xStart, yStart, xGoal, yGoal);
+		} catch (NoPathFoundException e) {
 			return null;
 		}
 		
-		return weightedAStar(xStart, yStart, xGoal, yGoal);
+		if (newTarget != null) {
+			tileMover.target = newTarget.times(16);
+		} else {
+			System.out.println("Goal reached!");
+			try {
+				goal = getGoal(gameData);
+			} catch (PlayerException | SpiNotFoundException e) {
+				return null;
+			}
+		}
+		
+		return tileMover.getDirection();
 	}
 	
 	/**
@@ -69,31 +136,21 @@ public class SmartAIMovementController implements MovementControllerSPI {
 	 * - @param yGoal the y-coordinate of the goal point
 	 * - @return the optimal direction for the entity to move
 	 */
-	private Direction weightedAStar(int xStart, int yStart, int xGoal, int yGoal) {
+	private Vector2 weightedAStar(int xStart, int yStart, int xGoal, int yGoal) throws NoPathFoundException {
 		State initialState = new State(xStart, yStart, xGoal, yGoal);
-		List<Node> path;
-		try {
-			path = treeSearch(initialState);
-		} catch (NoPathFoundException e) {
-			System.out.println(e);
-			return null;
-		}
+		List<Node> path = treeSearch(initialState);
 		
 		if (path.size() <= 1) {
 			return null;
 		}
 		
 		Node second = path.get(1);
-		Vector2 startPos = new Vector2(xStart, yStart);
-		Vector2 stepPos = new Vector2(second.state.x, second.state.y);
 		
-//		System.out.printf("X: %f Y: %f\n", stepPos.getX(), stepPos.getY());
-		
-		return startPos.getDirectionTo(stepPos);
+		return new Vector2(second.state.x, second.state.y);
 	}
 	
 	private List<Node> treeSearch(State initialState) throws NoPathFoundException {
-		Node initial_node = new Node(initialState, 0);
+		Node initial_node = new Node(initialState, 0, 1000);
 		PriorityQueue<Node> fringe = new PriorityQueue<>();
 		fringe.add(initial_node);
 		while (fringe.size() != 0) {
