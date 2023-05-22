1627a328b98b0dc3dd8cdaabf4e97679853470cb
Mikkel Albrechtsen <mikkel.alb@gmail.com>
2023-05-22 17:15:32 +0200

    Removed components not connected to this pull request

--- SmartAI/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-
-module SmartAI {
-    requires Common;
-    requires java.desktop;
-
-    exports dk.sdu.sesem4.smartAIMovement;
-    provides dk.sdu.sesem4.common.SPI.MovementControllerSPI with dk.sdu.sesem4.smartAIMovement.SmartAIMovementController;
-    provides dk.sdu.sesem4.common.SPI.ControlSPI with dk.sdu.sesem4.smartAIMovement.SmartAIMovement;
-}
--- SmoothRandomMovement/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module SmoothRandomMovement {
-    requires Common;
-    requires java.desktop;
-
-    exports dk.sdu.sesem4.SmoothRandomMovement;
-    provides dk.sdu.sesem4.common.SPI.MovementControllerSPI with dk.sdu.sesem4.SmoothRandomMovement.SmoothRandomMovementController;
-    provides dk.sdu.sesem4.common.SPI.ControlSPI with dk.sdu.sesem4.SmoothRandomMovement.SmoothRandomMovement;
-}
